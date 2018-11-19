9a7dfe6d03d236028cdd047b82edf8a74fa37adf
Stride <stride-admin@Strides-MacBook-Pro.local>
2018-11-19 10:15:23 -0500

    finishing this react saga tutorial

---
+++ src/sagas/index.js
@@ -0,0 +1,5 @@
+export {itemPriceSaga} from "./itemPriceSaga";
+export { currentUserSaga } from './currentUserSaga'
+export { fetchCartSaga } from './fetchCartSaga'
+export { itemDetailsSaga } from './itemDetailsSaga'
+export { itemQuantitySaga } from './itemQuantitySaga'
--- src/utility/makeActionCreator.js
+++ src/utility/makeActionCreator.js
@@ -1,6 +1,7 @@
 export const makeActionCreator = (type, ...argNames) => {
     return function(...args) {
         let action = { type };
+        // argNames is something like "id" and args is the value.
         argNames.forEach((arg, index) => {
             action[argNames[index]] = args[index]
         });
